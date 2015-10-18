254530b7966dfb93aba2c4c24721cdad78142aa0
Melissa Johnson <melissajohnson@g.harvard.edu>
2015-10-17 22:32:40 -0500

    Added Laravel log viewer package

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,11 @@
                 font-family: 'Lato';
             }
 
+            h1 {
+                font-size: 48px;
+                font-weight: bold;
+            }
+
             .container {
                 text-align: center;
                 display: table-cell;
@@ -34,10 +39,7 @@
                 font-size: 96px;
             }
 
-            .subhead {
-                font-size: 48px;
-                font-weight: bold;
-            }
+            
         </style>
     </head>
     <body>
@@ -48,22 +50,23 @@
                 <p>Ever needed random paragraphs of text to assist with designing your site?</p>
                 <p>Ever wish you didn't have to think of random user attributes to test various user permissions?</p>
                 <p>You've come to the right spot. This page delivers easy-to-use and flexible tools to help with these common developer needs.</p>
-                <div class="subhead">Random Text Generator</div>
+                <h1>Random Text Generator</h1>
                 <p>This first tool is a lorem ipsum generator. In case you're not familiar with this, lorem ipsum is a series of characters and white spaces used to graphically illustrate elements of a document. Developers generally use these generators to ensure that their pages work with what ever text is dynamically added to a page.</p>
                 <p>
                     <form method="POST">
+                        Number of paragraphs&nbsp;
                         <select>
                             <option value=1 selected>1</option>
                             <option value=2>2</option>
                             <option value=3>3</option>
                             <option value=4>4</option>
                             <option value-5>5</option>
-                        </select>
+                        </select><br /><br />
                         <input type="submit" value = "Get Text">
                     </form>
                 </p>
-                <div class="subhead">Random User Generator</div>
-    <br>
+                <h1>Random User Generator</h1>
+                <br>
             </div>
         </div>
     </body>
