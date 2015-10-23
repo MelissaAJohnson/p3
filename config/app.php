4cbc44a3085ff833784aa25e3c83c87bad1a2b24
Melissa Johnson <melissajohnson@g.harvard.edu>
2015-10-23 09:40:38 -0500

    Debugbar changes

--- config/app.php
+++ config/app.php
@@ -198,6 +198,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
 
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+
     ],
 
 ];
